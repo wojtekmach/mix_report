c1a14b24ceec69936b99ba2638ffc2522d830894
Wojtek Mach <wojtek@wojtekmach.pl>
2017-06-05 00:24:22 +0200

    Improve report.ecto

--- lib/mix/tasks/report.ecto.ex
+++ lib/mix/tasks/report.ecto.ex
@@ -22,19 +22,13 @@ if Code.ensure_loaded?(Ecto) do
     def run(args) do
       {opts, _, _} = OptionParser.parse(args)
 
-      [ecto_dep] = Mix.Dep.loaded_by_name([:ecto], [])
-      {:ok, ecto_version} = ecto.status
-
-      [adapter_dep] = Mix.Dep.loaded_by_name(@recognized_adapters, [])
-      {:ok, adapter_version} = adapter_dep.status
-
       body = """
       ### Environment
 
       * Elixir & Erlang versions: Elixir #{System.version()}, OTP #{Mix.Report.otp_version()}
-      * Database and version (PostgreSQL 9.4, MongoDB 3.2, etc.):
-      * Ecto version: #{ecto_version}
-      * Database adapter and version: #{adapter_dep.app} #{adapter_version}
+      * Database and version: #{database_version()}
+      * Ecto version: #{ecto_version()}
+      * Database adapter and version: #{adapter_version()}
       * Operating system: #{Mix.Report.os_version()}
 
       ### Current behavior
@@ -44,7 +38,6 @@ if Code.ensure_loaded?(Ecto) do
       ### Expected behavior
 
 
-
       """
 
       if opts[:print] do
@@ -52,6 +45,26 @@ if Code.ensure_loaded?(Ecto) do
       else
         Mix.Report.open_issue(@repo_url, body)
       end
+    end
+
+    defp ecto_version do
+      # TODO: don't use private APIs!
+      [ecto_dep] = Mix.Dep.loaded_by_name([:ecto], [])
+      {:ok, ecto_version} = ecto_dep.status
+      ecto_version
+    end
+
+    # TODO: instead grab adapter from application env and based on that grab the dep
+    defp adapter_version do
+      [adapter_dep] = Mix.Dep.loaded([]) |> Enum.filter(& &1.app in @recognized_adapters)
+      {:ok, adapter_version} = adapter_dep.status
+
+      "#{adapter_dep.app} #{adapter_version}"
+    end
+
+    # TODO: perhaps Repo would include a callback to grab this information?
+    defp database_version do
+      "PostgreSQL 9.4 / MongoDB 3.2 / etc"
     end
   end
 end
